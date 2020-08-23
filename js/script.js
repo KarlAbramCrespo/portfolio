94eb401b6d6dc75b9d0e2fd2e11a47b28ceddb23
Karl <karlabramcrespo@gmail.com>
2020-08-23 16:09:13 +0800

    Pushed cover animation

---
+++ js/script.js
@@ -0,0 +1,47 @@
+var loadArr = [];
+var loadCtr = 0;
+var loadInterval;
+
+function init() {
+    loadFonts();
+    loadArr.push("loadFonts");
+    
+    loadInterval = setInterval(checkLoad, 500);
+}
+
+function loadFonts() {
+    WebFont.load({
+        google: {
+            families: ['Abril Fatface', 'Roboto']
+        },
+        active: function() {
+            loadCtr++;
+        }
+    });
+}
+
+function checkLoad() {
+    if(loadArr.length == loadCtr){
+        clearInterval(loadInterval);
+        start();
+    }
+}
+
+function start() {
+    console.log("Here we go!")
+    
+    animate();
+}
+
+function animate(){
+    var tl = new TimelineMax();
+    
+    tl.set(main, {
+        opacity: 1
+    })
+    
+    tl.to(coverScreen, 0.5, {
+        delay: 2,
+        y: "-100%"
+    })
+}
